e4463d4469cf9699bedcecb4a4a021e4db489c87
Pillow Panda <yparkera@gmail.com>
2023-11-30 18:35:03 -0500

    add images etc

--- size_exp_salary_parallel.py
+++ size_exp_salary_parallel.py
@@ -3,3 +3,9 @@ import plotly.express as px
 
 # pip install -U kaleido
 salary = pd.read_csv('salaries.csv', sep = '\t')
+
+fig = px.parallel_categories(dogs, dimensions = ['sex','age','size'],
+                            labels = {'age':'Age', 'sex':'Sex', 'size':'Size'},
+                            title = 'Characteristics of Adoptable Dogs',
+                            )
+fig.show()
